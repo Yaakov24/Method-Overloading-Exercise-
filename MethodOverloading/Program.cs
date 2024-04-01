d33378266b500c4f21ddd691a1db339312343d72
YaakovG <ygoldenbergyo@gmail.com>
2024-04-01 15:08:05 -0400

    I changed the names of the variables

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -7,12 +7,12 @@
             Console.WriteLine(Add(34,354));
             Console.WriteLine(Add(23.4m, 45.7m));
             Console.WriteLine("Write a number");
-           int num = int.Parse(Console.ReadLine());
+           int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Write a number");
-            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Is this money? say true or false");
-            bool num2 = bool.Parse(Console.ReadLine());
-            Console.WriteLine(Add(num, num1, num2));
+            bool isMoney = bool.Parse(Console.ReadLine());
+            Console.WriteLine(Add(num1, num2, isMoney));
         }
 
         public static int Add(int num1, int num2)
@@ -23,12 +23,12 @@
         {
            return num1 + num2;
         }
-        public static string Add(int num1, int num2, bool num3)
+        public static string Add(int num1, int num2, bool isMoney)
         {   int answer = num1 += num2;
           
             string letter = "";
             if (answer > 1) {  letter = "s"; }
-            if (num3 == true)
+            if (isMoney == true)
             {
               return  $"{answer}dollar{letter}";
             }
