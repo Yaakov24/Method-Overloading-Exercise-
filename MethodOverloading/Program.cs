7ea4a2b4fb2fca7950eafc34b1332e8fd4f2724a
YaakovG <ygoldenbergyo@gmail.com>
2024-04-01 15:02:15 -0400

    I returned it all to the Main

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,36 +4,37 @@
     {
         static void Main(string[] args)
         {
-            Add(34,354);
-            Add(23.4m, 45.7m);
+            Console.WriteLine(Add(34,354));
+            Console.WriteLine(Add(23.4m, 45.7m));
             Console.WriteLine("Write a number");
-           var num = int.Parse(Console.ReadLine());
+           int num = int.Parse(Console.ReadLine());
             Console.WriteLine("Write a number");
-            var num1 = int.Parse(Console.ReadLine());
+            int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Is this money? say true or false");
-            var num2 = bool.Parse(Console.ReadLine());
-            Add(num, num1, num2);
+            bool num2 = bool.Parse(Console.ReadLine());
+            Console.WriteLine(Add(num, num1, num2));
         }
 
-        public static void Add(int num1, int num2)
+        public static int Add(int num1, int num2)
         { 
-             Console.WriteLine(num1 + num2);
+           return  num1 + num2;
         }
-        public static void Add(decimal num1, decimal num2)
+        public static decimal Add(decimal num1, decimal num2)
         {
-            Console.WriteLine(num1 + num2);
+           return num1 + num2;
         }
-        public static void Add(int num1, int num2, bool num3)
-        {   var answer = num1 += num2;
-            var letter = "";
+        public static string Add(int num1, int num2, bool num3)
+        {   int answer = num1 += num2;
+          
+            string letter = "";
             if (answer > 1) {  letter = "s"; }
             if (num3 == true)
             {
-                Console.WriteLine($"{answer}dollar{letter}");
+              return  $"{answer}dollar{letter}";
             }
             else
             {
-                Console.WriteLine(answer);
+               return $"{answer}";
             }
         }
 
